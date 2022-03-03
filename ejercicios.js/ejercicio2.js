2affe83d6d5745def9f1e8042d7bc37f387d6d9d
Joseph <jfalmachi1@espe.edu.ec>
2022-03-03 09:14:38 -0500

    MI PARTE

---
+++ ejercicios.js/ejercicio2.js
@@ -0,0 +1,8 @@
+suma=0
+var num= parseInt(prompt('PONGA NUMERO---------'))
+while (num !=0) {
+    if (num>0) {
+        num= parseInt(prompt('PONGA NUMERO---------'))
+    }else
+    document.write("PUSO 0");
+}
