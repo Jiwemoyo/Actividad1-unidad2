2ce8c5bb0012f718f4b8299894cdc19329760672
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2022-03-03 11:26:45 -0500

    Update ejercicio6.js

--- ejercicios.js/ejercicio6.js
+++ ejercicios.js/ejercicio6.js
@@ -10,22 +10,22 @@ if (articulo >=0  && articulo <=100 ) {
     
     document.write(`El precio de su compra es de: ${precioTotal} <br>`)
     precioTotal=precioTotal-(precioTotal*0.02)
-    document.write(`Pero con el descuentos sera de: ${precioTotal}`)
+    document.write(`Pero con el descuento del 2% sera de: ${precioTotal}`)
 
 }else if(articulo >=101  && articulo <=250){
 
     document.write(`El precio de su compra es de: ${precioTotal} <br>`)
     precioTotal=precioTotal-(precioTotal*0.05)
-    document.write(`Pero con el descuentos sera de: ${precioTotal}`)
+    document.write(`Pero con el descuento del 5% sera de: ${precioTotal}`)
 
 }else if(articulo >=251  && articulo <=800){
     document.write(`El precio de su compra es de: ${precioTotal} <br>`)
     precioTotal=precioTotal-(precioTotal*0.08)
-    document.write(`Pero con el descuentos sera de: ${precioTotal}`)
+    document.write(`Pero con el descuento del 8% sera de: ${precioTotal}`)
 }else if(articulo >= 801) {
     document.write(`El precio de su compra es de: ${precioTotal} <br>`)
     precioTotal=precioTotal-(precioTotal*0.12)
-    document.write(`Pero con el descuentos sera de: ${precioTotal}`)
+    document.write(`Pero con el descuento del 12% sera de: ${precioTotal}`)
 }else{
     document.write("No se admiten valore negativos")
 }
