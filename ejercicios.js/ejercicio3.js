20cac86ac098154d258475aab2b45f0a6eae6508
Joseph <jfalmachi1@espe.edu.ec>
2022-03-03 09:48:36 -0500

    EJERCICIOS

--- ejercicios.js/ejercicio3.js
+++ ejercicios.js/ejercicio3.js
@@ -1,12 +1,21 @@
 var N = parseInt(prompt("CUANTAS NOTAS TIENE----"));
 var promedio=0;
-var Acumula=0;
+var Acumula=1;
+var cuenta;
+var notas;
+var suma_notas=0;
 
-while (N!==0) {
-    cuenta=cuenta+1
-    Acumula=Acumula+N
+if (N!==0) {
+    while(Acumula<=N){
+        notas=parseInt(prompt("DAME LA NOTA"+ Acumula));
+        suma_notas=suma_notas+notas
+        Acumula=Acumula+1  
+    }
     
-}
-promedio=Acumula/cuenta
-document.write("promedio"+promedio); 
+}else{
+    document.write("NO NOTAS MENORES O IGUALES A 0");
 
+}
+
+promedio=suma_notas/N;
+document.write("PROMEDIO "+ promedio);
