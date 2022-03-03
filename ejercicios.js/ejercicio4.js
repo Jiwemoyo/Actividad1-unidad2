350c09be9047a5efe03e15598a0aa2797b5215be
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2022-03-03 08:02:18 -0500

    agregue algo

---
+++ ejercicios.js/ejercicio4.js
@@ -0,0 +1,15 @@
+let num = prompt("ingrese numeros enteros hasta el 10")
+num=parent(num)
+
+if (num <=0 || num > 10) {
+    alert("Solo numeros ente 1 y 10")
+} else {
+    switch (num) {
+        case 1:
+           
+            break;
+    
+        default:
+            break;
+    }
+}
